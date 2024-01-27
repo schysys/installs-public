6ada5a901d1f3bdcbd4d0e62cd12054b8ae858fb
Ryan Barnds <schysys@users.noreply.github.com>
2024-01-27 02:42:49 -0800

    update params

--- std_parameters.ps1
+++ std_parameters.ps1
@@ -1,5 +1,14 @@
 param(
+    [string]$v,  # Version
     [switch]$s,  # Skip update
     [switch]$u,  # Update
     [switch]$y   # Auto-accept (Yes)
 )
+
+function Invoke-ScriptFromUrl {
+    param([string]$url, [string]$arguments)
+    $scriptContent = (Invoke-WebRequest $url -UseBasicParsing).Content
+    if (-not [string]::IsNullOrWhiteSpace($scriptContent)) {
+        Invoke-Expression "$scriptContent $arguments"
+    }
+}
