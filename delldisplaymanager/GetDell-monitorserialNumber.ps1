d9ee700077ba15583ffbdc0b9c90c64d1c0c01d5
Ryan Barnds <schysys@users.noreply.github.com>
2024-01-29 19:46:00 -0800

    serials

---
+++ delldisplaymanager/GetDell-monitorserialNumber.ps1
@@ -0,0 +1,19 @@
+﻿## This script will get the Serial # to use instead of the Windows monitor # 1`,2 , 3 ,4 
+
+function Decode {
+    If ($args[0] -is [System.Array]) {
+        [System.Text.Encoding]::ASCII.GetString($args[0])
+    }
+    Else {
+        "Not Found"
+    }
+}
+
+echo "Name, Serial"
+
+ForEach ($Monitor in Get-WmiObject WmiMonitorID -Namespace root\wmi) {  
+    $Name = Decode $Monitor.UserFriendlyName -notmatch 0
+    $Serial = Decode $Monitor.SerialNumberID -notmatch 0
+	
+    echo "$Name, $Serial"
+}
